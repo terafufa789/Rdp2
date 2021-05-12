91c551e3822ade2711fbb574509935c6f3a1f8b5
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2021-05-12 10:35:07 +0530

    Add files via upload

---
+++ bash/bin/header.php
@@ -0,0 +1,13 @@
+<?php
+	function head(){
+		require('colors.php');
+		system('clear');
+		echo $nbold."┌-=============================-\n";
+		
+		echo "=  $red.┌────────$nbold Please Move bash script To encryption folder then Enter name -\n";
+		echo "=  $red.┌────────$nbold BASH   encryption -\n";
+		echo "=  $red.└────$nbold BY Simple Hacker1      \n";
+		echo "=  $red.└─── $nbold VERSION 1.0     -\n";
+		echo "└-=============================-\n$RR";
+	}
+?>
